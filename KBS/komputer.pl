b5d40e13cca3960ff133c6ccceb02df12a153ed2
Andre Susanto <andre@andresusanto.info>
2015-12-06 13:29:56 +0700

    Add several entities

--- KBS/komputer.pl
+++ KBS/komputer.pl
@@ -2,9 +2,22 @@
 
 	programming(programmer).
 	
+	baca_email(anggota_dpr).
+	baca_email(pelajar_smp).
+	baca_email(pelajar_sma).
+	baca_email(mahasiswa).
+	baca_email(programmer).
+	
+	internet(anggota_dpr).
+	internet(pelajar_smp).
+	internet(pelajar_sma).
+	internet(mahasiswa).
+	internet(programmer).
+	
 	grafis(programmer).
 	grafis(editor_video).
 	grafis(desainer).
+	grafis(pelajar_sma).
 	
 	video(editor_video).
 	
@@ -13,17 +26,21 @@
 	mengetik_dokumen(pelajar_smp).
 	mengetik_dokumen(pelajar_sma).
 	mengetik_dokumen(pelajar_sd).
+	mengetik_dokumen(anggota_dpr).
 
 % kebutuhan komputasi berdasarkan kegiatan
 
 	komputasi(X,Z) :- programming(X), Z = 300.
+	komputasi(X,Z) :- baca_email(X), Z = 100.
+	komputasi(X,Z) :- internet(X), Z = 150.
 	komputasi(X,Z) :- grafis(X), Z = 500.
 	komputasi(X,Z) :- video(X), Z = 500.
 	komputasi(X,Z) :- mengetik_dokumen(X), Z = 10.
 	
 % Kelas Komputer
 
-	kelas(a,X) :- aggregate(sum(P), komputasi(X,P), Total), Total >= 1000.
-	kelas(b,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 1000, Total >= 500.
-	kelas(c,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 500.
+	kelas(1,X) :- aggregate(sum(P), komputasi(X,P), Total), Total >= 1000.
+	kelas(2,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 1000, Total >= 500.
+	kelas(3,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 500, Total >= 200.
+	kelas(4,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 200.
 	
