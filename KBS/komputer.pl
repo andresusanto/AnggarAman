fcae2c49500fc12207043b74cc33a3a7d23061de
Andre Susanto <andre@andresusanto.info>
2015-12-06 13:15:47 +0700

    Refactor

---
+++ KBS/komputer.pl
@@ -0,0 +1,29 @@
+% penggunaan komputer oleh masing-masing pengguna
+
+	programming(programmer).
+	
+	grafis(programmer).
+	grafis(editor_video).
+	grafis(desainer).
+	
+	video(editor_video).
+	
+	mengetik_dokumen(sekertaris).
+	mengetik_dokumen(mahasiswa).
+	mengetik_dokumen(pelajar_smp).
+	mengetik_dokumen(pelajar_sma).
+	mengetik_dokumen(pelajar_sd).
+
+% kebutuhan komputasi berdasarkan kegiatan
+
+	komputasi(X,Z) :- programming(X), Z = 300.
+	komputasi(X,Z) :- grafis(X), Z = 500.
+	komputasi(X,Z) :- video(X), Z = 500.
+	komputasi(X,Z) :- mengetik_dokumen(X), Z = 10.
+	
+% Kelas Komputer
+
+	kelas(komputer,a,X) :- aggregate(sum(P), komputasi(X,P), Total), Total >= 1000.
+	kelas(komputer,b,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 1000, Total >= 500.
+	kelas(komputer,c,X) :- aggregate(sum(P), komputasi(X,P), Total), Total < 500.
+	
